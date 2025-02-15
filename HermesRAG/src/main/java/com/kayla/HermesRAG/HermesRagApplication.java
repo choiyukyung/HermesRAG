23ecb471477d1fc0aaa649ff348bd446d5ddedfd
choiyukyung <121738855+choiyukyung@users.noreply.github.com>
2025-02-15 13:45:12 +0900

    Initial Spring Boot project setup

---
+++ HermesRAG/src/main/java/com/kayla/HermesRAG/HermesRagApplication.java
@@ -0,0 +1,13 @@
+package com.kayla.HermesRAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermesRagApplication.class, args);
+	}
+
+}
---
+++ HermesRAG/src/test/java/com/kayla/HermesRAG/HermesRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayla.HermesRAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
