371eb651fa5686f7da31c1b8ff01d92ed97edc3e
choiyukyung <121738855+choiyukyung@users.noreply.github.com>
2025-03-24 22:50:20 +0900

    Limit search results last month in Qdrant

--- HermesRAG/src/python/similarity_search.py
+++ HermesRAG/src/python/similarity_search.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
 from typing import List, Tuple, Dict, Any
-from qdrant_client import QdrantClient
+from qdrant_client import QdrantClient, models
+from datetime import datetime, timedelta
 
 class SimilaritySearcher:
     def __init__(self, client: QdrantClient, collection_name: str = 'articles', model_name: str = 'paraphrase-multilingual-mpnet-base-v2'):
@@ -12,23 +13,39 @@ class SimilaritySearcher:
         # 쿼리 벡터화
         query_vector = self.model.encode(query).astype("float32").tolist()
 
+        # 최근 한 달
+        one_month_ago = datetime.now() - timedelta(days=30)
+        one_month_ago_timestamp = int(one_month_ago.timestamp()) # LocalDateTime 형식
+
         # Qdrant에서 유사도 검색
-        search_result = self.client.search(
+        search_result = self.client.query_points(
             collection_name=self.collection_name,
-            query_vector=query_vector,
+            query=query_vector,
             limit=top_n,
+            query_filter=models.Filter(
+                must=[
+                    models.FieldCondition( # 날짜 필터링: 한 달 이내의 데이터만 검색
+                        key="web_publication_date",
+                        range=models.DatetimeRange(
+                            gte=one_month_ago_timestamp,
+                            lte=int(datetime.now().timestamp())
+                        ),
+                    )
+                ]
+            ),
+            with_payload=True
         )
 
         results = []
-        for result in search_result:
-            similarity = 1 / (1 + result.score)  # L2 거리를 0~1 사이의 유사도로 변환
+        for ScoredPoint in search_result.points:
+            similarity = 1 / (1 + ScoredPoint.score) # L2 거리를 0~1 사이의 유사도로 변환
 
             results.append({
-                "id": result.payload.get("article_id", ""),
+                "id": ScoredPoint.payload.get("article_id", ""),
                 "similarity": round(float(similarity), 4),
-                "web_title": result.payload.get("web_title", ""),
-                "trail_text": result.payload.get("trail_text", ""),
-                "web_url": result.payload.get("web_url", "")
+                "web_title": ScoredPoint.payload.get("web_title", ""),
+                "trail_text": ScoredPoint.payload.get("trail_text", ""),
+                "web_url": ScoredPoint.payload.get("web_url", "")
             })
 
         return results
